749b8175e161b9b791f91dc86f4384aa9c9e0cfa
bykeny <kenanramaznov@gmail.com>
2026-02-05 00:42:43 +0200

    Add backend configuration for Azure in Terraform provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 3.100"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-streakly-tfstate"
+    storage_account_name = "streaklytfstate70938"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
